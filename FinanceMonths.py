1e81fafe866304a3b3b28c8551e50719672896d4
saulflrs <japasneezemonkey@yahoo.com>
2020-03-04 23:47:09 -0600

    imported finance module to a new script on new file

--- FinanceMonths.py
+++ FinanceMonths.py
@@ -5,14 +5,14 @@ from pandas import Series, DataFrame
 
 #variables to set the start time to start pulling historical data from stocks and apprpiate index
 start = datetime.datetime(2010, 1, 11)
-end = datetime.datetime(2020, 3, 3)
+end = datetime.datetime(2020, 3, 4)
 #stocks that I am interersted in 
 stocks = ['AAPL', 'SBUX', 'GOOG', 'VZ', 'MSFT','STAG','WPG','UNP','FB','AXP','SPY','VTI','VOO']
-
+stocks1 = ['^DJI']
 #data = web.get_data_yahoo(stocks,'01/01/2010',interval='m')['Adj Close']
 #only want the Adj Close
-data = web.get_data_yahoo(stocks,start,interval='m')['Adj Close']
+data = web.get_data_yahoo(stocks1,start,interval='m')['Adj Close']
 
 #data.head(50).mean()
 #I want a CSV file that I can use for excel 
-data.to_csv(r'/Users/saul/Downloads/Finance8.csv',)
+data.to_csv(r'/Users/saul/Documents/GitHub/Stock-Data/Finance11.csv',)
---
+++ impor_ FinanceMonths.py
@@ -0,0 +1,6 @@
+#I just learned that any py file can be imported as a module to be run in 
+#another python script  
+
+import FinanceMonths
+
+print(FinanceMonths)
